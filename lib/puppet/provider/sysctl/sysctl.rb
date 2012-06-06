954c3f2f3db6bd8382435918bebc2a8eb152f883
trlinkin <trlinkin@gmail.com>
2012-06-06 17:10:21 -0400

    Expanding out provider, adding logic for parsed file.

--- lib/puppet/provider/sysctl/sysctl.rb
+++ lib/puppet/provider/sysctl/sysctl.rb
@@ -1,10 +1,73 @@
+require 'puppet/provider/parsedfile'
 
 sysctl_conf = "/etc/sysctl.conf"
 
 Puppet::Type.type(:sysctl).provide(:sysctl,:parent => Puppet::Provider::ParsedFile,
   :default_target => sysctl_conf,:filetype => :flat) do
-  confine :exists => sysctl_conf
 
+  #confine :exists => sysctl_conf  # Not sure if confining to this file is smart, by default, mac has one that is .default
   commands :sysctl => "sysctl"
 
+
+  # "Borrowed" from the 'host' type
+  text_line :comment, :match => /^#/
+  text_line :blank, :match => /^\s*$/
+
+
+  # define our record line so that the sysctl may be parsed
+  record_line :parsed,
+              :fields     => %W{comment param value},
+              :optional   => %{comment}
+              :match      => /(^\s+#.*)?\n^([\w\d_\.]+)[\s=:]+(\S+)$/,
+              :post_parse => proc { |hash|
+                # Again, the 'host' type was used as an example here...
+                # An absent comment should match "comment => ''"
+                hash[:comment] = '' if hash[:comment].nil? or hash[:comment] == :absent
+              }
+              :to_line    => proc { |hash|
+                [:param, :value].each do |key|
+                  raise ArgumentError, "#{key} is a require attribut for sysctl" unless hash[key] and hash[key] != absent
+                end
+
+                # BSD and Linux/Darwin have a different style for their sysctl.conf entries
+                os = Facter.value(:operatingsystem)
+                case
+                when os =~ /bsd/i
+                  delim = ": "
+                else
+                  delim = " = "
+                end
+
+                str = ""
+                if hash.include?(:comment) and !hash[:comment].empty?
+                  str = "##{hash[:comment]}\n"
+                end
+                str += "#{hash[:param:]}#{delim}#{hash[:value]}"
+                str
+              }
+
+    def getparam(param)
+      if cmd = Puppet::Util::which('sysctl')
+        IO.popen(cmd+" -a") do |list|
+          list.each do |line|
+            result = line.split(/^([\w\d_\.]+)[\s=:]+(\S+)$/)
+            return result if result[1] == param
+          end
+        end
+      end
+      nil
+    end
+
+    def isparam?(param)
+      getparam(param)
+    end
+
+    def getvalue(param)
+      if getparam(param)
+
+      end
+    end
+
 end
+
+# @resource.fail
--- lib/puppet/type/sysctl.rb
+++ lib/puppet/type/sysctl.rb
@@ -3,39 +3,63 @@ module Puppet
     ensurable
 
     @doc = "Manages sysctl volues. This module will ensure the value of
-    	kernel settings in the sysctl configuration. It can also remove
-    	values so that they fall back on the system defaults. This module
-    	will optionally ensure that the value specified is the current 
-    	running value in the system. Naturally, this is not always a great
-    	idea. 
+      kernel settings in the sysctl configuration. It can also remove
+      values so that they fall back on the system defaults. This module
+      will optionally ensure that the value specified is the current 
+      running value in the system. Naturally, this is not always a great
+      idea. 
 
-    	As a general warning, please be aware you're messing with 
-    	internal settings of your kernel. It is not the fault of this module
-    	if you use it to make poorly researched desicions."
+      As a general warning, please be aware you're messing with internal 
+      settings of your kernel, possibly durring runtime!. It is not the 
+      fault of this type if you use it to make poorly researched desicions."
+
+#    feature :changesysctl, "The provider needs to modify the running kernel paramter values.",
+#      :methods => [:sysctlparam?, :sysctlparam=]
+
+#    newproperty(:enable, :required_features => :changesysctl) do
+#      desc "Set paramter of running system if it differs."
+#
+#      newvalue(:true)
+#      newValue(:false)
+
+      #defaultto :false
+#    end
+
+    newproperty(:target) do
+      desc "The file in which to store sysctl information. Only used when 
+        saving changes to disk. Defaults to `/etc/sysctl.conf`. Some newer
+        distrobutions of Linux now support a `sysctl.d` directory."
+
+      defaultto { 
+        if @resource.class.defaultprovider.ancestors.include?(Puppet::Provider::ParsedFile)
+          @resource.class.defaultprovider.default_target
+        else
+          nil
+        end
+      }
+    end
+
+    newproperty(:comment) do
+      desc "A comment that will be placed above the line with a # character."
+    end
 
     newparam(:param, :namevar => true) do
-        desc "Name of the kernel parameter."
+      desc "Name of the kernel parameter."
 
-        validate do |value|
-            unless value ~= /\w*\.?/ 
-                raise ArgumentError, "Parameter formatting is not valid."
-            end
+      validate do |value|
+        unless value =~ /^[\w\d_\.]+$/ 
+          raise ArgumentError, "Kernel parameter formatting is not valid."
         end
+      end
     end
 
     newparam(:value) do
-        desc "Value of the parameter."
-    end
-
-    newproperty(:enable) do
-        desc "Set paramter of running system if it differs."
-        newvalue(:true)
-        newValue(:false)
+      desc "Value of the kernel parameter."
     end
     
     autorequire(:file) do
-        ["/etc/sysctl.conf"]
+      ["/etc/sysctl.conf"]
     end
-
   end
 end
+
